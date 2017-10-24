7d602568f40d19c1cf0b5953928b4e66c8a53bcc
wwy0918 <wwy0918@163.com>
2017-10-24 18:25:33 +0800

    添加代理, 防止奔溃

--- __Y__/__Y__/NavViewController.m
+++ __Y__/__Y__/NavViewController.m
@@ -8,7 +8,7 @@
 
 #import "NavViewController.h"
 
-@interface NavViewController ()<UINavigationControllerDelegate>
+@interface NavViewController ()<UINavigationControllerDelegate, UIGestureRecognizerDelegate>
 @end
 
 @implementation NavViewController
@@ -19,6 +19,8 @@
     [self customPopGesture];
 }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincomplete-implementation"
 /// 自定义返回手势
 - (void)customPopGesture {
     // 获取系统自带返回手势代理
@@ -26,10 +28,16 @@
     // 创建自己的侧滑手势
     // handleNavigationTransition: 是系统自带的全屏手势响应方法,不需要自己去实现,系统已经实现好了
     UIPanGestureRecognizer *popGesture = [[UIPanGestureRecognizer alloc] initWithTarget:target action:@selector(handleNavigationTransition:)];
-//    popGesture.delegate = self;
+    popGesture.delegate = self;
     [self.view addGestureRecognizer:popGesture];
     // 禁用系统自带的全屏手势
     self.interactivePopGestureRecognizer.enabled = NO;
+}
+#pragma clang diagnostic pop
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    return self.viewControllers.count > 1;
 }
 
 /// hidesBottomBarWhenPushed
