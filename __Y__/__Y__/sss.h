8e177c47a3fe9f257ef8bcd8ebf3e50d004ddca8
wwy0918 <wwy0918@163.com>
2017-10-25 16:21:51 +0800

    2345yu

---
+++ __Y__/__Y__/sss.h
@@ -0,0 +1,13 @@
+//
+//  sss.h
+//  __Y__
+//
+//  Created by wwyun on 2017/10/25.
+//  Copyright © 2017年 wwy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sss : UIView
+
+@end
---
+++ __Y__/__Y__/sss.m
@@ -0,0 +1,21 @@
+//
+//  sss.m
+//  __Y__
+//
+//  Created by wwyun on 2017/10/25.
+//  Copyright © 2017年 wwy. All rights reserved.
+//
+
+#import "sss.h"
+
+@implementation sss
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
